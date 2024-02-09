291f21eee1120001aeaa84b237ed49197e99076e
hitesh bansal <h.hiteshbansal@gmail.com>
2024-02-09 22:28:52 +0530

    changes

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Username  from '@/backend/models/models' 
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOption } from '../api/auth/[...nextauth]/route';
 
-const Admin = () => {
+const Admin = async(req) => {
+const session = await getServerSession(authOption);
+  const id = req.searchParams.id;
+const data = await Username.findById(id) 
+if(!session || session.user.email !== data.email){
+redirect('/');  
+}
+
   return (
-    <div>Admin</div>
+    <div>{data.username}</div>
   )
 }
 
--- src/components/home_utility/homeForm/form.js
+++ src/components/home_utility/homeForm/form.js
@@ -21,7 +21,7 @@ else if(preferedlink.length>0){
     <>
     <form className="flex flex-row items-center" onSubmit={funsubmit}>
   <span className='bg-white text-black py-3 ps-6'>linklist.to/</span>
-  <input type="text" placeholder="username"  className='py-3 text-black'/>
+  <input type="text" placeholder="username"  className='py-3 text-black focus:border-slate-50'/>
   <button className='bg-emerald-600 px-5 py-3 text-black border-none select-none' type='submit'>Sign in Free</button>
 </form>
     </>
