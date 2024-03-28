c88b31ef46097f7318677cc022b7f5adb8b3a5d6
Hitesh_Bansal <h.hiteshbansal@gmail.com>
2024-03-29 00:30:21 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
         },
         {
           hostname: 'i.pravatar.cc'
+        },
+        {
+          hostname:"lh3.googleusercontent.com"
         }
         ],
       },
