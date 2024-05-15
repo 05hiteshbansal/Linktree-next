56d4cc88807ad25aa741da6e7f23db974c1ae512
Hitesh_Bansal <h.hiteshbansal@gmail.com>
2024-05-15 13:36:04 +0530

    changes

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   console.log(loginImg)
   return (
     <div className='flex flex-col md:flex-row items-center h-full'>
-    <div className='h-full w-full'>
+    <div className='h-full w-full hidden md:block'>
         <Image src={loginImg} className='h-screen w-full' placeholder="blur" alt='loginimg'/>
     </div>
       <div className='w-full flex flex-col items-center justify-center'>
